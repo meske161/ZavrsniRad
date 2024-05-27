f6752aabe514201d81697334cffb28cbb5f15b89
Ivan Mesarić <mesaritchievan@gmail.com>
2024-05-27 21:25:03 +0200

    Insert podataka u bazu

---
+++ insert.sql
@@ -0,0 +1,22 @@
+﻿select * from Bands;
+insert into Bands (BandName,Genre)
+values ('Smrt Razuma','hardcore punk/crossover thrash');
+
+insert into Bands (BandName,Genre) values 
+('Proslov', 'hardcore metal'),
+('Merrow', 'groove/stoner metal'),
+('Finger Trigger Happiness', 'grunge'),
+('Zagrobni Čučanj', 'crnjak punk'),
+('AB Nekultura', 'anarcho punk'),
+('Kill Sustain', 'metal'),
+('Gunslingers Bride', 'groove/stoner metal');
+
+select * from Users;
+insert into Users (FirstName,LastName,Email,Band) values
+('Ivan','Branšteter','istvahc@gmail.com','5'),
+('Domagoj','Talan','tale777@gmail.com','6'),
+('Nenad','Dujmić','nenoduja84@gmail.com','7'),
+('Igor','Atlija','atlijaigorrr@gmail.com','8'),
+('Ivan','Branšteter','istvahc@gmail.com','9'),
+('Marko','Vujković','vujkam@gmail.com','10'),
+('Marko','Vujković','vujkam@gmail.com','11');
