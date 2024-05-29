edd21cdcc337c03822307841d2b0a331fe5c2695
Ivan Mesarić <mesaritchievan@gmail.com>
2024-05-29 17:32:51 +0200

    Dodavanje insert naredbe glavnoj (uvodnoj) skripti

--- rehearsals.sql
+++ rehearsals.sql
@@ -38,3 +38,26 @@ Reserved bit
 alter table Users add foreign key (Band) references Bands (ID);
 alter table Reservations add foreign key (BandID) references Bands (ID);
 alter table Reservations add foreign key (RoomID) references Rooms (ID);
+
+insert into Bands (BandName,Genre) values 
+('Smrt Razuma','hardcore punk/crossover thrash'),
+('Proslov', 'hardcore metal'),
+('Merrow', 'groove/stoner metal'),
+('Finger Trigger Happiness', 'grunge'),
+('Zagrobni Čučanj', 'crnjak punk'),
+('AB Nekultura', 'anarcho punk'),
+('Kill Sustain', 'metal'),
+('Gunslingers Bride', 'groove/stoner metal');
+
+insert into Users (FirstName,LastName,Email,Band) values
+('Krunoslav','Kajfeš','kkajfes79@gmail.com','1'),
+('Ivan','Branšteter','istvahc@gmail.com','2'),
+('Domagoj','Talan','tale777@gmail.com','3'),
+('Nenad','Dujmić','nenoduja84@gmail.com','4'),
+('Igor','Atlija','atlijaigorrr@gmail.com','5'),
+('Ivan','Branšteter','istvahc@gmail.com','6'),
+('Marko','Vujković','vujkam@gmail.com','7'),
+('Marko','Vujković','vujkam@gmail.com','8');
+
+-- select * from Bands;
+-- select * from Users;
